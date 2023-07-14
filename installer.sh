ec4cb6f399fd0b3385acbcf640c116013ac6b54e
Çağan Başer <108457913+c0x164@users.noreply.github.com>
2023-07-14 11:50:58 +0300

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,7 @@
+echo "Hello"
+
+sudo dnf copr enable sunwire/envycontrol
+
+sudo dnf install bspwm sxhkd polybar cava neofetch dunst flameshot git firefox python3-envycontrol wget pamixer sddm rofi alacritty mpd picom
+
+
