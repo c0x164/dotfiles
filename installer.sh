44339cf5e223c9cda578ec0c43bd0df2eeff5a5a
Çağan Başer <108457913+c0x164@users.noreply.github.com>
2023-07-15 20:35:50 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,2 +1,4 @@
 echo "Hello"
 sudo dnf install bspwm sxhkd polybar cava neofetch dunst flameshot git firefox python3-envycontrol wget pamixer sddm rofi alacritty mpd picom
+
+exec dbus-launch --exit-with-session bspwm
