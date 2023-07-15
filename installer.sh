92888de14145969d2d009993ba20d3f767e5a4cc
Çağan Başer <108457913+c0x164@users.noreply.github.com>
2023-07-15 20:33:13 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,68 @@
 echo "Hello"
 
-sudo dnf copr enable sunwire/envycontrol
-
-sudo dnf install bspwm sxhkd polybar cava neofetch dunst flameshot git firefox python3-envycontrol wget pamixer sddm rofi alacritty mpd picom
-
-
+sudo dnf install \
+ @"base-x" \
+ @"Common NetworkManager Submodules" \
+ @"Fonts" \
+ @"Hardware Support" \
+ bluedevil \
+ breeze-gtk \
+ breeze-icon-theme \
+ cagibi \
+ colord-kde \
+ cups-pk-helper \
+ dolphin \
+ glibc-all-langpacks \
+ gnome-keyring-pam \
+ kcm_systemd \
+ kde-gtk-config \
+ kde-partitionmanager \
+ kde-print-manager \
+ kde-settings-pulseaudio \
+ kde-style-breeze \
+ kdegraphics-thumbnailers \
+ kdeplasma-addons \
+ kdialog \
+ kdnssd \
+ kf5-akonadi-server \
+ kf5-akonadi-server-mysql \
+ kf5-baloo-file \
+ kf5-kipi-plugins \
+ khotkeys \
+ kmenuedit \
+ konsole5 \
+ kscreen \
+ kscreenlocker \
+ ksshaskpass \
+ ksysguard \
+ kwalletmanager5 \
+ kwebkitpart \
+ kwin \
+ NetworkManager-config-connectivity-fedora \
+ pam-kwallet \
+ phonon-qt5-backend-gstreamer \
+ pinentry-qt \
+ plasma-breeze \
+ plasma-desktop \
+ plasma-desktop-doc \
+ plasma-drkonqi \
+ plasma-nm \
+ plasma-nm-l2tp \
+ plasma-nm-openconnect \
+ plasma-nm-openswan \
+ plasma-nm-openvpn \
+ plasma-nm-pptp \
+ plasma-nm-vpnc \
+ plasma-pa \
+ plasma-user-manager \
+ plasma-workspace \
+ plasma-workspace-geolocation \
+ polkit-kde \
+ qt5-qtbase-gui \
+ qt5-qtdeclarative \
+ sddm \
+ sddm-breeze \
+ sddm-kcm \
+ setroubleshoot \
+ sni-qt \
+ xorg-x11-drv-libinput
